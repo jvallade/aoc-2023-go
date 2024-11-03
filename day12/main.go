99549e64a243c2f7dc0cdc7882e421a74ad6ad7c
Julien Vallade <julien.vallade@cnes.Fr>
2024-11-03 18:28:31 +0100

    add memoization

--- day12/main.go
+++ day12/main.go
@@ -9,29 +9,6 @@ import (
 	"strings"
 )
 
-type Group struct {
-	start      int
-	end        int
-	nextGroups []Group
-	valid      bool
-}
-
-// Define custom error types
-type GroupSizeMismatch struct{}
-type GroupNotFound struct{}
-type FoundUnexpectedGroup struct{}
-
-// Implement the Error method for each custom error type
-func (e GroupSizeMismatch) Error() string {
-	return "group size mismatch"
-}
-func (e GroupNotFound) Error() string {
-	return "group not found"
-}
-func (e FoundUnexpectedGroup) Error() string {
-	return "found unexpected group"
-}
-
 func main() {
 	inputFile, err := os.Open("day12/input.txt")
 	if err != nil {
@@ -51,6 +28,7 @@ func main() {
 
 	totalCount := CountArrangements(input, unfold)
 	fmt.Println("total possible arrangements :", totalCount)
+	fmt.Println("memo hits :", memoHits)
 }
 
 func Unfold(springs, groups string) (string, string) {
@@ -63,150 +41,87 @@ func Unfold(springs, groups string) (string, string) {
 	return totSprings, totGroups
 }
 
-func FindNextPossibleGroup(springs string, groupSizes []int, offset int) ([]Group, error) {
-	inGroup := false
-	var start int
+type MemoInput struct {
+	springs    string
+	groupSizes string
+}
+
+func CreateMemoInput(springs string, groupSizes []int) MemoInput {
+	sizes := ""
+	for _, size := range groupSizes {
+		sizes += strconv.Itoa(size) + ","
+	}
+	return MemoInput{springs, sizes}
+}
+
+var memoHits int = 0
+
+func FindNextPossibleGroup(springs string, groupSizes []int, memo map[MemoInput]int) int {
 
 	// fmt.Println("finding new group in", springs)
 	// fmt.Println("searching next group size", groupSizes)
 	// fmt.Println("offset", offset)
 
-	groups := make([]Group, 0)
-
-	for i, c := range springs {
-		switch c {
-		case '.':
-			// we found a complete group...
-			if inGroup {
-				// ... but we did not expect a new group (i.e groupSizes is empty)
-				if len(groupSizes) == 0 {
-					// we should not reach this point
-					log.Fatal("we should not reach this point - this should be handled at group start")
-				}
-
-				// now we need to check if the group we found has the expected size
-				if i-start == groupSizes[0] {
-					// we found a group with the expected size
-					group := Group{start + offset, i + offset, make([]Group, 0), true}
-
-					// now we need to find the next possible groups from this one
-					nextGroups, _ := FindNextPossibleGroup(springs[i+1:], groupSizes[1:], offset+i+1)
-					for _, g := range nextGroups {
-						if g.valid {
-							group.nextGroups = append(group.nextGroups, g)
-						}
-					}
-					groups = append(groups, group)
-					return groups, nil
-				} else {
-					// we found a group but it does not have the expected size
-					group := Group{valid: false}
-					groups = append(groups, group)
-					return groups, nil
-				}
-			}
-		case '#':
-			if len(groupSizes) == 0 {
-				// we found a group but we did not expect any group
-				group := Group{valid: false}
-				groups = append(groups, group)
-				return groups, nil
-			}
-
-			if !inGroup {
-				inGroup = true
-				start = i
-			}
-		case '?':
-			if inGroup {
-				if i-start > groupSizes[0] {
-					// we found a group but it is too big
-					group := Group{valid: false}
-					groups = append(groups, group)
-					return groups, nil
-
-				} else if i-start == groupSizes[0] {
-					group := Group{start + offset, i + offset, make([]Group, 0), true}
-
-					// now we need to find the next possible groups from this one
-					nextGroups, _ := FindNextPossibleGroup(springs[i+1:], groupSizes[1:], offset+i+1)
-					for _, g := range nextGroups {
-						if g.valid {
-							group.nextGroups = append(group.nextGroups, g)
-						}
-					}
-					groups = append(groups, group)
-					return groups, nil
-				}
-			} else {
-				if len(groupSizes) == 0 {
-					// we do not expect any group - we can skip this character
-					continue
-				}
-
-				// we start by skipping this character (i.e. we search a group starting at the next character)
-				nextGroups, _ := FindNextPossibleGroup(springs[i+1:], groupSizes, offset+i+1)
-				for _, g := range nextGroups {
-					if g.valid {
-						groups = append(groups, g)
-					}
-				}
-
-				// then we start a new group
-				start = i
-				inGroup = true
-			}
-		}
-	}
-
-	if inGroup {
-		if len(springs)-start == groupSizes[0] {
-			group := Group{start + offset, len(springs) + offset, make([]Group, 0), true}
-			if len(groupSizes) == 1 {
-				// we found all the groups we were looking for
-				groups = append(groups, group)
-				return groups, nil
-			} else {
-				// we found a group but we did not find all the groups we were looking for
-				group.valid = false
-				groups = append(groups, group)
-				return groups, nil
-			}
-		} else {
-			group := Group{valid: false}
-			groups = append(groups, group)
-			return groups, nil
-		}
-	}
-
 	if len(groupSizes) == 0 {
-		// we did not find any group but we did not expect any group
-		return groups, nil
-	} else {
-		// we did not find any group but we did expect a group
-		group := Group{valid: false}
-		groups = append(groups, group)
-		return groups, nil
-	}
-}
-
-func FlattenGroup(group Group) [][]Group {
-	if len(group.nextGroups) == 0 {
-		return [][]Group{{group}}
-	}
-
-	groups := make([][]Group, 0)
-	for _, g := range group.nextGroups {
-		flattenedGroups := FlattenGroup(g)
-		for _, fg := range flattenedGroups {
-			groups = append(groups, append([]Group{group}, fg...))
+		// we do not expect any group
+		if !strings.Contains(springs, "#") {
+			return 1
 		}
+		// fmt.Println("no group left")
+		return 0
 	}
-	return groups
+
+	memoInput := CreateMemoInput(springs, groupSizes)
+	if val, ok := memo[memoInput]; ok {
+		// fmt.Println("found in memo")
+		memoHits++
+		return val
+	}
+
+	requiredSize := 0
+	for _, size := range groupSizes {
+		requiredSize += size
+	}
+	requiredSize += len(groupSizes) - 1
+	if len(springs) < requiredSize {
+		// fmt.Println("not enough springs left")
+		return 0
+	}
+
+	// we can search from the next character
+	if springs[0] == '.' {
+		return FindNextPossibleGroup(springs[1:], groupSizes, memo)
+	}
+
+	currentGroupSize := groupSizes[0]
+	allBrokenSprings := !strings.ContainsAny(springs[:currentGroupSize], ".")
+	var lastCharValid bool
+	if len(springs) == currentGroupSize {
+		lastCharValid = true
+	} else {
+		// the character after the group must be a '.' or a '?'
+		lastCharValid = springs[currentGroupSize] != '#'
+	}
+
+	arrangementCount := 0
+
+	if allBrokenSprings && lastCharValid {
+		nextIndex := min(currentGroupSize+1, len(springs))
+		arrangementCount += FindNextPossibleGroup(springs[nextIndex:], groupSizes[1:], memo)
+	}
+
+	// we need to check starting one char further as this '?' could be a '.'
+	if springs[0] == '?' {
+		arrangementCount += FindNextPossibleGroup(springs[1:], groupSizes, memo)
+	}
+
+	memo[memoInput] = arrangementCount
+	return arrangementCount
 }
 
 func CountArrangements(input *bufio.Scanner, unfold bool) int {
-	totalCount := 0
+	globalCounter := 0
+	memo := make(map[MemoInput]int)
 	for input.Scan() {
 		line := input.Text()
 		parts := strings.Fields(line)
@@ -232,23 +147,9 @@ func CountArrangements(input *bufio.Scanner, unfold bool) int {
 			}
 			groupSizes[i] = size
 		}
-
-		foundGroups, err := FindNextPossibleGroup(springs, groupSizes, 0)
-		if err != nil {
-			fmt.Println(foundGroups, err)
-			continue
-		}
-
-		count := 0
-		for _, g := range foundGroups {
-			fgs := FlattenGroup(g)
-			for _, fg := range fgs {
-				if len(fg) == len(expectedGroups) {
-					count++
-				}
-			}
-		}
-		totalCount += count
+		count := FindNextPossibleGroup(springs, groupSizes, memo)
+		fmt.Println("count", count)
+		globalCounter += count
 	}
-	return totalCount
+	return globalCounter
 }
